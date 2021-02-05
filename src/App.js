8cd14bb3b88b912aca249945803eed6dc4702f4d
PaburoTC <pablotcampos25@gmail.com>
2021-02-05 11:43:35 +0100

    Fixing GH Pages refresh bug

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter, Switch, Route} from "react-router-dom";
+import {HashRouter, Switch, Route} from "react-router-dom";
 import Main from "./components/main/Main";
 import Register from "./components/register/Register";
 import Login from "./components/login/Login";
@@ -7,13 +7,13 @@ import Login from "./components/login/Login";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter basename="/">
         <Switch>
             <Route path="/register" component={Register} exact/>
             <Route path="/login" component={Login} exact/>
             <Route path="/" component={Main}/>
         </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
