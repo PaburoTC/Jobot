7c3e71b266d55cefd0d406f8934f16abf21135d3
PaburoTC <pablotcampos25@gmail.com>
2021-02-06 11:26:44 +0100

    Connected to backend

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {HashRouter, Switch, Route} from "react-router-dom";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 import Main from "./components/main/Main";
 import Register from "./components/register/Register";
 import Login from "./components/login/Login";
@@ -7,13 +7,13 @@ import Login from "./components/login/Login";
 
 function App() {
   return (
-    <HashRouter basename="/">
+    <BrowserRouter>
         <Switch>
             <Route path="/register" component={Register} exact/>
             <Route path="/login" component={Login} exact/>
             <Route path="/" component={Main}/>
         </Switch>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
