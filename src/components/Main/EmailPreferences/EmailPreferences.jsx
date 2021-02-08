8300bb08f99f598b0e331cc9ce7d3844443398b8
PaburoTC <pablotcampos25@gmail.com>
2021-02-08 09:43:09 +0100

    Added EmailPreferences

---
+++ src/components/Main/EmailPreferences/EmailPreferences.jsx
@@ -0,0 +1,108 @@
+import React, {useState, useEffect, useRef} from 'react';
+import axios from "axios";
+import './EmailPreferences.css';
+
+const EmailPreferences = props =>{
+    const didMount = useRef(false)
+
+    const [emailMonday, setEmailMonday] = useState(false)
+    const [emailTuesday, setEmailTuesday] = useState(false)
+    const [emailWednesday, setEmailWednesday] = useState(false)
+    const [emailThursday, setEmailThursday] = useState(false)
+    const [emailFriday, setEmailFriday] = useState(false)
+    const [emailSaturday, setEmailSaturday] = useState(false)
+    const [emailSunday, setEmailSunday] = useState(false)
+
+    useEffect(()=>{
+        axios.get(`https://api.jobot.es/profile/${props.user}`)
+            .then(response => {
+                if(response.data.exists){
+                    setEmailMonday(response.data.profile[0])
+                    setEmailTuesday(response.data.profile[1])
+                    setEmailWednesday(response.data.profile[2])
+                    setEmailThursday(response.data.profile[3])
+                    setEmailFriday(response.data.profile[4])
+                    setEmailSaturday(response.data.profile[5])
+                    setEmailSunday(response.data.profile[6])
+                }
+            })
+    }, [props.user])
+
+    useEffect(()=>{
+        if(didMount.current){
+            axios.post(`https://api.jobot.es/profile/${props.user}`,
+                {
+                    0:emailMonday,
+                    1:emailTuesday,
+                    2:emailWednesday,
+                    3:emailThursday,
+                    4:emailFriday,
+                    5:emailSaturday,
+                    6:emailSunday
+                })
+                .then(() => {
+                }, error => {
+                    console.log(error)
+                })
+        }else{
+            didMount.current = true;
+        }
+    },[props.user, emailMonday,emailTuesday,emailWednesday,emailThursday,emailFriday,emailSaturday,emailSunday])
+
+    return(
+        <div id="email-preferences">
+            <h2>Quiero recibir notificaciones de eventos que caigan en</h2>
+            <div className="switch-container">
+                <label className="switch">
+                    <input type="checkbox" name="0" onChange={event =>setEmailMonday(event.target.checked)} checked={emailMonday}/>
+                    <span className="slider"/>
+                </label>
+                <div>Lunes</div>
+            </div>
+            <div className="switch-container">
+                <label className="switch">
+                    <input type="checkbox" name="1" onChange={event => setEmailTuesday(event.target.checked)} checked={emailTuesday}/>
+                    <span className="slider"/>
+                </label>
+                <div>Martes</div>
+            </div>
+            <div className="switch-container">
+                <label className="switch">
+                    <input type="checkbox" name="2" onChange={event => setEmailWednesday(event.target.checked)} checked={emailWednesday}/>
+                    <span className="slider"/>
+                </label>
+                <div>Miércoles</div>
+            </div>
+            <div className="switch-container">
+                <label className="switch">
+                    <input type="checkbox" name="3" onChange={event => setEmailThursday(event.target.checked)} checked={emailThursday}/>
+                    <span className="slider"/>
+                </label>
+                <div>Jueves</div>
+            </div>
+            <div className="switch-container">
+                <label className="switch">
+                    <input type="checkbox" name="4" onChange={event => setEmailFriday(event.target.checked)} checked={emailFriday}/>
+                    <span className="slider"/>
+                </label>
+                <div>Viernes</div>
+            </div>
+            <div className="switch-container">
+                <label className="switch">
+                    <input type="checkbox" name="5" onChange={event => setEmailSaturday(event.target.checked)} checked={emailSaturday}/>
+                    <span className="slider"/>
+                </label>
+                <div>Sábado</div>
+            </div>
+            <div className="switch-container">
+                <label className="switch">
+                    <input type="checkbox" name="6" onChange={event => setEmailSunday(event.target.checked)} checked={emailSunday}/>
+                    <span className="slider"/>
+                </label>
+                <div>Domingo</div>
+            </div>
+        </div>
+    )
+}
+
+export default EmailPreferences;
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,7 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import  {setCookie,getCookie} from '../../cookie_manager.js'
 import './Main.css'
-import axios from "axios";
+import EmailPreferences from "./EmailPreferences/EmailPreferences";
 
 const Main = props =>{
     const [currentUser] = useState(getCookie('current_user'))
@@ -10,45 +10,6 @@ const Main = props =>{
         props.history.replace('/Login')
     }
 
-    const [emailMonday, setEmailMonday] = useState(false)
-    const [emailTuesday, setEmailTuesday] = useState(false)
-    const [emailWednesday, setEmailWednesday] = useState(false)
-    const [emailThursday, setEmailThursday] = useState(false)
-    const [emailFriday, setEmailFriday] = useState(false)
-    const [emailSaturday, setEmailSaturday] = useState(false)
-    const [emailSunday, setEmailSunday] = useState(false)
-
-    useEffect(()=>{
-        axios.get(`https://api.jobot.es/profile/${currentUser}`)
-            .then(response => {
-                if(response.data.exists){
-                    setEmailMonday(response.data.profile[0])
-                    setEmailTuesday(response.data.profile[1])
-                    setEmailWednesday(response.data.profile[2])
-                    setEmailThursday(response.data.profile[3])
-                    setEmailFriday(response.data.profile[4])
-                    setEmailSaturday(response.data.profile[5])
-                    setEmailSunday(response.data.profile[6])
-                }
-            })
-    }, [currentUser])
-
-    useEffect(()=>{
-        axios.post(`https://api.jobot.es/profile/${currentUser}`,
-            {
-                0:emailMonday,
-                1:emailTuesday,
-                2:emailWednesday,
-                3:emailThursday,
-                4:emailFriday,
-                5:emailSaturday,
-                6:emailSunday
-            })
-            .then(() => {
-            }, error => {
-                console.log(error)
-            })
-    },[currentUser,emailMonday,emailTuesday,emailWednesday,emailThursday,emailFriday,emailSaturday,emailSunday])
 
     const logout = ()=>{
         console.log("POG")
@@ -59,34 +20,7 @@ const Main = props =>{
     return(
         <div>
             <button onClick={logout}>Cerrar sesión</button>
-            <label className="switch">
-                <input type="checkbox" name="0" onChange={event =>setEmailMonday(event.target.checked)} checked={emailMonday}/>
-                <span className="slider"/>
-            </label>
-            <label className="switch">
-                <input type="checkbox" name="1" onChange={event => setEmailTuesday(event.target.checked)} checked={emailTuesday}/>
-                <span className="slider"/>
-            </label>
-            <label className="switch">
-                <input type="checkbox" name="2" onChange={event => setEmailWednesday(event.target.checked)} checked={emailWednesday}/>
-                <span className="slider"/>
-            </label>
-            <label className="switch">
-                <input type="checkbox" name="3" onChange={event => setEmailThursday(event.target.checked)} checked={emailThursday}/>
-                <span className="slider"/>
-            </label>
-            <label className="switch">
-                <input type="checkbox" name="4" onChange={event => setEmailFriday(event.target.checked)} checked={emailFriday}/>
-                <span className="slider"/>
-            </label>
-            <label className="switch">
-                <input type="checkbox" name="5" onChange={event => setEmailSaturday(event.target.checked)} checked={emailSaturday}/>
-                <span className="slider"/>
-            </label>
-            <label className="switch">
-                <input type="checkbox" name="6" onChange={event => setEmailSunday(event.target.checked)} checked={emailSunday}/>
-                <span className="slider"/>
-            </label>
+            <EmailPreferences user={currentUser}/>
         </div>
     )
 }
