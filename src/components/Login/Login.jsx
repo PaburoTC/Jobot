b975470386a4a1162b32acb4202ccabad53c42d1
PaburoTC <pablotcampos25@gmail.com>
2021-03-12 19:21:42 +0100

    Implementing CSRF protection

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -43,7 +43,7 @@ const Login = props =>{
         });
     }
 
-    const googleLogin = response =>{
+    const googleLogin = async response =>{
         axios.post('https://jobot.es/api/auth/googleLogin',
             {
                 token: response.tokenId
